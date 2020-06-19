5e236b4c665417997bf37d53422f72757709d718
matcracker <matcracker98@gmail.com>
2020-06-19 12:55:52 +0200

    Accidentaly removed the buttons initilization...

--- src/matcracker/FormLib/Form.php
+++ src/matcracker/FormLib/Form.php
@@ -38,6 +38,7 @@ class Form extends BaseForm
         parent::__construct($onSubmit, $onClose);
         $this->setType(self::FORM_TYPE);
         $this->setMessage("");
+        $this->data["buttons"] = [];
     }
 
     /**
